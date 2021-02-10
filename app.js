8e5d408469fe6e09623c678b5667166ebb1751ce
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2021-02-11 00:56:35 +0900

    A

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
 const { app, BrowserWindow, Tray, Menu } = require('electron');
 
 app.on('ready', () => {
-    iconPath = __dirname + "/icon.ico"
-    mainWindow = new BrowserWindow({
-        icon: iconPath,
-        webPreferences: {
-            webSecurity: false
-        }
-    });
-    mainWindow.getBounds
-    mainWindow.loadURL('https://music.youtube.com/');
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    });
-    tray = new Tray(iconPath);
-    tray.setToolTip("YouTube Music desktop app.")
+  iconPath = __dirname + "/icon.ico"
+  mainWindow = new BrowserWindow({
+    icon: iconPath,
+    webPreferences: {
+      webSecurity: false
+    }
+  });
+  mainWindow.getBounds
+  mainWindow.loadURL('https://music.youtube.com/');
+  mainWindow.on('closed', () => {
+    app.quit()
+    mainWindow = null
+  });
+  tray = new Tray(iconPath);
+  tray.setToolTip("YouTube Music desktop app.")
 
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Autohide',
-            type: "checkbox",
-            checked: autohide,
-            click(menuItem) {
-                autohide = menuItem.checked
-                if (!autohide) mainWindow.show()
-            }
-        }, {
-            type: 'separator'
-        }, {
-            label: 'Quit',
-            role: "close",
-            accelerator: "Q",
-            click(menuItem) {
-                app.quit();
-            }
-        }
-    ])
-    tray.setContextMenu(contextMenu)
+  const contextMenu = Menu.buildFromTemplate([{
+    label: 'Quit',
+    role: "close",
+    accelerator: "Q",
+    click() {
+      app.quit();
+    }
+  }])
+  tray.setContextMenu(contextMenu)
 });
